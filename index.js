f8102d50ebb8fb2922843af3d85da835da31ed0a
vd0904 <vedantdwivedi094@gmail.com>
2024-02-01 11:07:35 +0530

    formatted

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const simpleGit = require('simple-git');
 
 const FILE_PATH = './data.json';
 
-const DATE = moment().subtract(79, 'd').format();
+const DATE = moment().subtract(80, 'd').format();
 
 const data = {
     date: DATE
